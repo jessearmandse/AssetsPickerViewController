54f28c2520349527003cf4e8bb9cf3c28e984d71
Junfeng Hu <jh@tectusdreamlab.com>
2019-08-02 10:18:20 +0800

    Add AssetsPicker project

---
+++ AssetsPickerViewController/AssetsPicker.h
@@ -0,0 +1,18 @@
+//
+//  AssetsPicker.h
+//  AssetsPicker
+//
+//  Created by Junfeng Hu on 2/8/19.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AssetsPicker.
+FOUNDATION_EXPORT double AssetsPickerVersionNumber;
+
+//! Project version string for AssetsPicker.
+FOUNDATION_EXPORT const unsigned char AssetsPickerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AssetsPicker/PublicHeader.h>
+
+
